0956b195e156d193fe85251efb762b2c0accedb8
신동규 <donggyu9410@gmail.com>
2022-10-03 19:55:17 +0900

    feat: 프로젝트 생성

---
+++ DisneyPlus/DisneyPlus/DisneyPlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  DisneyPlusApp.swift
+//  DisneyPlus
+//
+//  Created by 신동규 on 2022/10/03.
+//
+
+import SwiftUI
+
+@main
+struct DisneyPlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
