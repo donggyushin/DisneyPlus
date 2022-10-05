c83c75e8a4d314dd7a328076273a0be6257bb41a
donggyu <donggyu_00@naver.com>
2022-10-06 00:04:09 +0900

    [Feature] 3강. Custom Tab View (#4)
    * feat: TabViewRouter 생성
    
    * feat: tabBarIcon component 생성
    
    * feat: Custom Tab 생성
    
    * feat: 미리보기 생성

---
+++ DisneyPlus/DisneyPlus/views/ProfileView.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileView.swift
+//  DisneyPlus
+//
+//  Created by 신동규 on 2022/10/05.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text("Profile")
+    }
+}
+
+struct ProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileView()
+    }
+}
---
+++ DisneyPlus/DisneyPlus/views/SearchView.swift
@@ -0,0 +1,20 @@
+//
+//  SearchView.swift
+//  DisneyPlus
+//
+//  Created by 신동규 on 2022/10/05.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    var body: some View {
+        Text("Search")
+    }
+}
+
+struct SearchView_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchView()
+    }
+}
