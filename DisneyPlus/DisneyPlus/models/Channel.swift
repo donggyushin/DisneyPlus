b8f05644ab5268ecbc2b319312bf5c518cda3c3c
신동규 <donggyu9410@gmail.com>
2022-10-08 01:19:32 +0900

    feat: channel, searchCategory, profile

---
+++ DisneyPlus/DisneyPlus/models/Channel.swift
@@ -0,0 +1,16 @@
+//
+//  Channel.swift
+//  DisneyPlus
+//
+//  Created by 신동규 on 2022/10/08.
+//
+
+import Foundation
+
+enum Channel: String, CaseIterable {
+    case disney
+    case pixar
+    case marvel
+    case sw
+    case ng
+}
---
+++ DisneyPlus/DisneyPlus/models/SearchCategory.swift
@@ -0,0 +1,14 @@
+//
+//  SearchCategory.swift
+//  DisneyPlus
+//
+//  Created by 신동규 on 2022/10/08.
+//
+
+import Foundation
+
+struct SearchCategory: Identifiable {
+    let id = UUID()
+    var title: String
+    var systemImage: String
+}
