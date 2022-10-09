4f129d5ea8c402adfd2b2b51c21d92a891d999aa
신동규 <donggyu9410@gmail.com>
2022-10-09 18:29:59 +0900

    feat: logoheaderview

---
+++ DisneyPlus/DisneyPlus/components/LogoHeaderView.swift
@@ -0,0 +1,25 @@
+//
+//  LogoHeaderView.swift
+//  DisneyPlus
+//
+//  Created by 신동규 on 2022/10/09.
+//
+
+import SwiftUI
+
+struct LogoHeaderView: View {
+    var body: some View {
+        Image("logo")
+            .resizable()
+            .scaledToFit()
+    }
+}
+
+struct LogoHeaderView_Previews: PreviewProvider {
+    static var previews: some View {
+        ZStack {
+            GradientBackgroundView()
+            LogoHeaderView()
+        }
+    }
+}
